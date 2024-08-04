4564928439407d841005de5bc633d1933340612f
Eadweard <aurora.coder.zqf@gmail.com>
2024-08-04 16:30:00 +0800

    ok

--- standard-library/context_test.go
+++ standard-library/context_test.go
@@ -1,13 +1,33 @@
 package standardLibrary
 
-import ("testing" "context" "time")
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestContext(t *testing.T) {
 	ctx := context.Background() // 起点
-  timeoutCtx, cancel := context.WithTimeout(ctx, time.Second)
+	timeoutCtx, cancel := context.WithTimeout(ctx, time.Second)
+
+	defer cancel()
+	end := make(chan struct{}, 1)
+	ch := timeoutCtx.Done()
+
+	go func() {
+		Sleep2s()
+		end <- struct{}{}
+	}()
+	select {
+	case <-ch:
+		fmt.Println("timeout")
+	case <-end:
+		fmt.Println("end")
+	}
+
 }
 
-func SomeBusiness(ctx: context.Context) {
-
+func Sleep2s() {
+	time.Sleep(2 * time.Second)
 }
-
